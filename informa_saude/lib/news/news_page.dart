7e101513cf0cdcbadbbb2eb00248bd29975cc612
Larissa Bernardon <larissabernardon@Larissas-MacBook-Pro.local>
2022-05-15 15:43:15 -0300

    adicionar secoes noticias e twitter

--- informa_saude/lib/news/news_page.dart
+++ informa_saude/lib/news/news_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+enum CardType {
+  twitter,
+  fakeNews,
+  covid,
+  sintomas,
+  transmissao,
+}
+
 class NewsPage extends StatelessWidget {
   const NewsPage({Key? key}) : super(key: key);
   static const IconData coronavirusOutlined =
@@ -9,117 +17,19 @@ class NewsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildTitleTwitter(),
-            _buildCardsTwitter(),
-            _buildTitleNoticias(),
-            _buildCardsNoticias(),
-            _buildTitleCovid19(),
-            _buildCards(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildCards() {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) => _buildCardItem(index),
-      ),
-    );
-  }
-
-  Widget _buildCardsTwitter() {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (context, index) => _buildCardItemTwitter(index),
-      ),
-    );
-  }
-
-  Widget _buildCardsNoticias() {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (context, index) => _buildCardItemNoticias(index),
-      ),
-    );
-  }
-
-  Padding _buildCardItemTwitter(int index) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      child: Container(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
-              width: 30,
-            ),
-            _buildIcon(index),
-            const SizedBox(width: 15),
-            _buildCardTitleTwitter(index),
-          ],
-        ),
-        height: 115,
-        width: 360,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: const Color.fromRGBO(190, 202, 218, 1),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Text _buildCardTitleTwitter(int index) {
-    return Text(
-      index == 0 ? "O que é COVID-19?" : "",
-      style: const TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.w700,
-      ),
-    );
-  }
-
-  Padding _buildCardItemNoticias(int index) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      child: Container(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) => Teste()));
-          },
-          child: Text("Ir para Segunda Página"),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
-              width: 30,
-            ),
-            _buildIcon(index),
-            const SizedBox(width: 15),
-            _buildCardTitleNoticias(index),
-          ],
-        ),
-        height: 115,
-        width: 360,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: const Color.fromRGBO(190, 202, 218, 1),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSectionTitle('Twitter oficial'),
+              _buildCardItem(type: CardType.twitter),
+              _buildSectionTitle('Notícias'),
+              _buildCardItem(type: CardType.fakeNews),
+              _buildSectionTitle('COVID-19'),
+              _buildCardItem(type: CardType.covid),
+              _buildCardItem(type: CardType.sintomas),
+              _buildCardItem(type: CardType.transmissao),
+            ],
           ),
         ),
       ),
@@ -127,51 +37,67 @@ class NewsPage extends StatelessWidget {
   }
 
   Text _buildCardTitleNoticias(int index) {
-    return Text(
-      index == 0 ? "O que é COVID-19?" : "",
-      style: const TextStyle(
+    return const Text(
+      'Notícias',
+      style: TextStyle(
         fontSize: 18,
         fontWeight: FontWeight.w700,
       ),
     );
   }
 
-  Padding _buildCardItem(int index) {
+  Padding _buildCardItem({required CardType type}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      child: InkWell(
-          onTap: () {
-            print("tapped on container");
-          },
-          child: Container(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 30,
-                ),
-                _buildIcon(index),
-                const SizedBox(width: 15),
-                _buildCardTitle(index),
-              ],
-            ),
-            height: 115,
-            width: 360,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: const Color.fromRGBO(190, 202, 218, 1),
+      child: GestureDetector(
+        onTap: () {
+          print("tapped on container");
+        },
+        child: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 30,
               ),
+              _buildIcon(type),
+              const SizedBox(width: 15),
+              _buildCardTitle(type),
+            ],
+          ),
+          height: 115,
+          width: 360,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: const Color.fromRGBO(190, 202, 218, 1),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 
-  Text _buildCardTitle(int index) {
+  Text _buildCardTitle(CardType type) {
+    switch (type) {
+      case CardType.covid:
+        return _buildTitle('O que é COVID-19?');
+      case CardType.sintomas:
+        return _buildTitle('Quais são os sintomas?');
+      case CardType.transmissao:
+        return _buildTitle('Como se transmite?');
+      case CardType.twitter:
+        return _buildTitle('Ministério da saúde');
+      case CardType.fakeNews:
+        return _buildTitle('Fake news?');
+    }
+  }
+
+  Text _buildTitle(String text) {
     return Text(
-      index == 0 ? "O que é COVID-19?" : "Quais são os sintomas?",
+      text,
       style: const TextStyle(
         fontSize: 18,
         fontWeight: FontWeight.w700,
@@ -179,22 +105,57 @@ class NewsPage extends StatelessWidget {
     );
   }
 
-  Container _buildIcon(int index) {
+  Container _buildIcon(CardType type) {
+    switch (type) {
+      case CardType.covid:
+        return _buildCardInfo(
+          const Color.fromRGBO(209, 210, 255, 1),
+          coronavirusOutlined,
+          const Color.fromRGBO(108, 110, 219, 1),
+        );
+      case CardType.sintomas:
+        return _buildCardInfo(
+          const Color.fromRGBO(214, 246, 255, 1),
+          Icons.vaccines_outlined,
+          const Color.fromRGBO(0, 157, 199, 1),
+        );
+      case CardType.transmissao:
+        return _buildCardInfo(
+          const Color.fromRGBO(242, 227, 233, 1),
+          Icons.update_outlined,
+          const Color.fromRGBO(157, 76, 108, 1),
+        );
+      case CardType.twitter:
+        return _buildCardInfo(
+          const Color.fromRGBO(242, 227, 233, 1),
+          Icons.add_to_home_screen_outlined,
+          const Color.fromRGBO(157, 76, 108, 1),
+        );
+      case CardType.fakeNews:
+        return _buildCardInfo(
+          const Color.fromRGBO(250, 240, 219, 1),
+          Icons.newspaper_outlined,
+          const Color.fromRGBO(224, 159, 31, 1),
+        );
+    }
+  }
+
+  _buildCardInfo(
+    Color color,
+    IconData iconData,
+    Color iconColor,
+  ) {
     return Container(
       width: 56,
       height: 56,
       decoration: BoxDecoration(
-        color: index == 0
-            ? const Color.fromRGBO(209, 210, 255, 1)
-            : const Color.fromRGBO(214, 246, 255, 1),
+        color: color,
         borderRadius: BorderRadius.circular(40),
       ),
       child: Icon(
-        coronavirusOutlined,
+        iconData,
         size: 30,
-        color: index == 0
-            ? const Color.fromRGBO(108, 110, 219, 1)
-            : const Color.fromRGBO(0, 157, 199, 1),
+        color: iconColor,
       ),
     );
   }
@@ -219,12 +180,15 @@ class NewsPage extends StatelessWidget {
     );
   }
 
-  Padding _buildTitleNoticias() {
-    return const Padding(
-      padding: EdgeInsets.fromLTRB(30, 30, 30, 10),
+  Padding _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 30, 30, 10),
       child: Text(
-        'Notícias',
-        style: TextStyle(fontSize: 27, fontWeight: FontWeight.w600),
+        title,
+        style: const TextStyle(
+          fontSize: 27,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
